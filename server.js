c5e8959f39eea3d7811db001ac9dc31fbebba802
Dr.Redfish <drredfish@protonmail.com>
2021-10-10 15:29:52 +0400

    [ADD]  port in node serv

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var http = require('http'); // Import Node.js core module
+var port = 5595;
 
 var server = http.createServer(function (req, res) {   //create web server
     if (req.url == '/') { //check the URL of the current request
@@ -30,6 +31,6 @@ var server = http.createServer(function (req, res) {   //create web server
 
 });
 
-server.listen(5000); //6 - listen for any incoming requests
+server.listen(port); //6 - listen for any incoming requests
 
-console.log('Node.js web server at port 5000 is running..')
+console.log('Node.js web server at '+port+' is running..')
