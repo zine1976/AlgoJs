be2702e7136f7f5dbec113f14f8670f597da8437
femknight <emiliefiliondonato.1@gmail.com>
2021-10-14 14:36:46 +0200

    [fix] espacement des variables

--- var.js
+++ var.js
@@ -1,18 +1,30 @@
 var start = "HelloWorld In Js From Script";
-var page='<html><body><p id="demo"></p></body></html>';
+var page='<html><body><p id="demo">page</p></body></html>';
 console.log(start,page);
 
-// getElementById('demo').innerHTML= page;
+document.getElementById('demo').innerHTML= page;
 
 
+
+console.log("var.js");
 /* Variables Emilie */
+
+/* Variable em */
 var em= 5;
-console.log("variable em is:" em);
+console.log("variable em is:", em);
+
+/* Variable is */
 var is= em + 2;
-console.log("variable is is:" is);
+console.log("variable is is:", is);
+
+/* Variable aw */
 var aw = is * 4;
-console.log("variable aw is:" aw);
+console.log("variable aw is:", aw);
+
+/* Variable some */
 var some = aw / 4;
-console.log("variable some is:" some);
+console.log("variable some is:", some);
+
+/* Variable ness */
 var ness = some - 2;
-console.log("variable ness is:" ness);
+console.log("variable ness is:", ness);
