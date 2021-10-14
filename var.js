8190fc1c15d18454a4ab6f7cbd2507719a532db6
Your Name <christian.tudoroiu@gmail.com>
2021-10-14 11:46:29 +0200

    ajout tableaux

--- var.js
+++ var.js
@@ -9,9 +9,20 @@ x *= 3;
 x -= 2;
 x /= 2; 
 x += z;
-console.log(x);
+
+// les tableaux
+let tab=[];
+let tab2=[];
+// facon 1 de definir 
+tab[0]="mazda";
+tab[1]="ford";
+// facon 2 de definir
+tab2=["voiture1", "voiture2"];
+
+console.log(x,tab,tab2);
 
 document.getElementById('demo').innerHTML= x;
+document.write(tab," ",tab2);
 
 
-// 
+
