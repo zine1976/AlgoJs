59cfba0bc94d68839a3073622a7c2932691e9ba1
Dr.Redfish <drredfish@protonmail.com>
2021-10-14 14:50:30 +0400

    [ADD] intro event

--- var.js
+++ var.js
@@ -38,3 +38,8 @@ console.log("The last modif " + etagere)
 
 
 
+var clicked = document.getElementById("clicked").onclick =
+function(e){
+    console.log("clicked: " + clicked)
+    alert("you have clicked ?")
+}
