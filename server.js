8825a370a3b866fd5a834cbef333946b135341fa
Dr.Redfish <drredfish@protonmail.com>
2021-10-14 12:40:05 +0400

    [ADD] patecode intro variable

--- server.js
+++ server.js
@@ -28,6 +28,13 @@ var server = http.createServer(function (req, res) {   //create web server
         res.end();
 
     }
+    else if (req.url == "/DWWM19") {
+
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>La page des bests.</p></body></html>');
+        res.end();
+
+    }
     else
         res.end('Invalid Request!');
 
--- var.js
+++ var.js
@@ -1,4 +1,34 @@
-var start = "HelloWorld In Js From Script";
-var page='<p>Le Javascript</p>';
-console.log(start,page);
-document.getElementById('demo').innerHTML= page;
+
+// variables :
+// code du formateur
+
+var x = 42
+var page= " en js" 
+var result = x + page
+console.log(result);
+document.getElementById('demo').innerHTML= result;
+
+
+
+///PSEUDO-code explicatif (non-fonctionnel en javascript) algo des pates
+
+var pate = "pate"
+var eau = "eau"
+var feux = "feux"
+var casserole= "casserole"
+
+
+if casserole & eau & pate {
+    
+    if feux {
+        put eau + casserole
+    }
+
+    if eau + casserole + feux = True {
+        wait 5 min
+    }
+
+    if(5 min = true){
+        return patte mangeable
+    }
+}
