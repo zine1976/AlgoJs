16adb97b9a8c6b2d0f6dcb81004dad8c64f1de85
Your Name <christian.tudoroiu@gmail.com>
2021-10-14 11:25:17 +0200

    nouvelle modif exo-variables

--- var.js
+++ var.js
@@ -1,14 +1,14 @@
-// Code de Cristian
-var un = "Salut";
-var deux = un + "bonjour";
-var trois = 15;
-var quatre = trois - 5;
-var cinq = quatre * 9;
-var six = cinq / 3;
-var sept = six +2;
-console.log(un+" ",deux+" ",trois+" ",quatre+" ",cinq+" ",six+" ",sept+" ");
 
-document.getElementById('demo').innerHTML= un+" "+deux+" "+trois+" "+quatre+" "+cinq+" "+six+" "+sept+" ";
-document.write("utilisation de write "+sept+" ", six+" ", cinq);
+// Code de Cristian
+var x = 15;
+x += 15;
+x *=3;
+x -=2;
+x /=2; 
+x += "Salut";
+console.log(x);
+
+document.getElementById('demo').innerHTML= x;
+
 
 // 
