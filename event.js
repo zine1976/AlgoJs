1d9716ec33b21cd81cd972314be05ce5b1aa17cd
Dr.Redfish <drredfish@protonmail.com>
2021-10-14 15:02:23 +0400

    [ADD] event onclick

---
+++ event.js
@@ -0,0 +1,5 @@
+var clicked = document.getElementById("clicked").onclick =
+function(e){
+    console.log("clicked: " + clicked)
+    alert("you have clicked ?")     
+}
--- var.js
+++ var.js
@@ -41,5 +41,5 @@ console.log("The last modif " + etagere)
 var clicked = document.getElementById("clicked").onclick =
 function(e){
     console.log("clicked: " + clicked)
-    alert("you have clicked ?")
+    alert("you have clicked ?")     
 }
