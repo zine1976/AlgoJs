c3ab12241f5097e512824cccdd157704d12d434d
femknight <emiliefiliondonato.1@gmail.com>
2021-10-14 11:24:38 +0200

    les cinq variables d'Emilie

--- var.js
+++ var.js
@@ -3,3 +3,11 @@ var page='<html><body><p id="demo"></p></body></html>';
 console.log(start,page);
 
 // getElementById('demo').innerHTML= page;
+
+
+/* Variables Emilie */
+var em= 5;
+var is= em + 2;
+var aw = is * 4;
+var some = aw / 4;
+var ness = some - 2;
