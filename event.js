f17f661b8a520f698813041cb455b694b41cfe38
femknight <emiliefiliondonato.1@gmail.com>
2021-10-14 14:37:30 +0200

    je ne sais plus c'est quelle modif

--- event.js
+++ event.js
@@ -1,7 +1,7 @@
 
-console.log("envent.js");
-var clicked = document.getElementById("clicked").onclick = ;
+console.log("event.js");
+var clicked = document.getElementById("clicked").onclick =
 function(e) {
     console.log("clicked:"+clicked);
-    alert("you have clicked")
-}
+    alert("you have clicked");
+};
