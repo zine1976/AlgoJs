eb31e62b8fdd4fe78f7542c8b6d802161da3edf7
Your Name <christian.tudoroiu@gmail.com>
2021-10-14 11:27:12 +0200

    modif de l'exo

--- var.js
+++ var.js
@@ -1,11 +1,14 @@
 
 // Code de Cristian
 var x = 15;
+var z = " Salut";
+var y = " DWWM19 "
+z += y;
 x += 15;
-x *=3;
-x -=2;
-x /=2; 
-x += "Salut";
+x *= 3;
+x -= 2;
+x /= 2; 
+x += z;
 console.log(x);
 
 document.getElementById('demo').innerHTML= x;
