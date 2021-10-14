cd446847c97295e7ed72788318a3e42420e72978
femknight <emiliefiliondonato.1@gmail.com>
2021-10-14 12:10:46 +0200

    console logs

--- var.js
+++ var.js
@@ -7,7 +7,12 @@ console.log(start,page);
 
 /* Variables Emilie */
 var em= 5;
+console.log("variable em is:" em);
 var is= em + 2;
+console.log("variable is is:" is);
 var aw = is * 4;
+console.log("variable aw is:" aw);
 var some = aw / 4;
+console.log("variable some is:" some);
 var ness = some - 2;
+console.log("variable ness is:" ness);
