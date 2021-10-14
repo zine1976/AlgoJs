99158c661346ad5daec7e23593a8741581219b86
Gregor <failyriece@gmail.com>
2021-10-14 14:06:42 +0200

    [ADD] RAINBOW!!

--- var.js
+++ var.js
@@ -27,5 +27,32 @@ function ColorDice(){
     document.getElementById('button').style.backgroundColor = color;
 }
 
+var mode = 1;
+var rouge = 0;
+var vert = 255;
+var bleu = 0;
 
+function ColorResult(){
+    if (mode == 1) {
+        vert -= 1;
+        bleu += 1;
+    }else if (mode == 2) {
+        bleu -= 1;
+        rouge +=1;
+    } else {
+        vert += 1;
+        rouge -=1;
+    }
+    if (vert == 255 && bleu == 0 && rouge == 0) {
+        mode = 1;
+    }else if (vert ==0 && bleu == 255 && rouge == 0) {
+        mode = 2;
+    } else if (vert ==0 && bleu == 0 && rouge == 255){
+        mode = 3;
+    }
+    var couleur = "rgb("+rouge+", "+vert+", "+bleu+")"
+    change.style.color = couleur;
+}
+
+setInterval(ColorResult, 1)
 // getElementById('demo').innerHTML= page;
