87a524968a71475c6b5c6e42ce4714910c180f20
PB <ypapov@gmail.com>
2021-10-14 14:29:59 +0200

    pas vraiment un easter egg

--- var.js
+++ var.js
@@ -1,5 +1,28 @@
-var start = "HelloWorld In Js From Script";
-var page='<html><body><p id="demo"></p></body></html>';
-console.log(start,page);
+var log = "Voici le résultat :"
 
-// getElementById('demo').innerHTML= page;
+var x = 15
+var y = -25
+
+x = y
+
+console.log(log, x);
+
+// modif 2
+
+var z = " string "
+x = x + z
+
+console.log("modif2" + log + x)
+
+var fkg =document.getElementById('demo');
+console.log(fkg)
+fkg.innerHTML = log + z + x
+
+
+// variable clicked
+
+var clicked = document.getElementById('clicked').onclick =
+function(e){
+    console.log("clicked :" +clicked)
+    alert("mdr bien vu Neo")
+}
