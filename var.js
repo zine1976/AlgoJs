616573a1323835b695db802a7b8660fc84a5fd6a
Michel <vilbertmichel9@gmail.com>
2021-10-14 13:19:13 +0200

    exoo

--- var.js
+++ var.js
@@ -1,5 +1,45 @@
-var start = "HelloWorld In Js From Script";
-var page='<html><body><p id="demo"></p></body></html>';
-console.log(start,page);
+// var start = "HelloWorld In Js From Script";
+// var page='<html><body><p id="demo"></p></body></html>';
+// console.log(start,page);
+
+
 
 // getElementById('demo').innerHTML= page;
+
+
+// 1
+
+var log = "test"
+var x = 9
+y = 50
+x = x + y
+console.log( "1" + log + x )
+
+// 2
+
+var z = "test"
+x = x + y + z
+console.log ("2" + log + x )
+
+// 3
+
+x = y - z
+console.log ("3" + log + x )
+
+// 4 
+
+x = y
+console.log ("4" + log + x )
+
+// 5
+
+x = 5
+console.log ("5" + log + x )
+
+
+
+var az = document.getElementById('demo');
+
+console.log(az)
+
+az.innerHTML = z
