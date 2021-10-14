d33688ba6f52e96109a245836683ede3eaa65bfb
Your Name <christian.tudoroiu@gmail.com>
2021-10-14 17:14:15 +0200

    tableaux

--- array.js
+++ array.js
@@ -9,5 +9,14 @@ const string = ["rouge","vert","bleu"]
 const integer = [21,42,84]
 const bool = [true, false, null]
 const variable = [g1,g2,g3]
+const histoire = ["Bonjour", "le ","groupe", "DWWM19"];
 
 
+document.getElementById("tab1").innerHTML = string;
+document.getElementById("tab2").innerHTML = integer;
+document.getElementById("tab3").innerHTML = bool;
+document.getElementById("tab4").innerHTML = variable;
+
+for(let variable in histoire ){
+    document.write(variable);
+}
