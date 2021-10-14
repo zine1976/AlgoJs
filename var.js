64ba0ffc3159661df6214da1f0a184e2ca0498a3
Your Name <christian.tudoroiu@gmail.com>
2021-10-14 11:09:06 +0200

    exo-variable Cristian

--- var.js
+++ var.js
@@ -1,5 +1,14 @@
-var start = "HelloWorld In Js From Script";
-var page='<html><body><p id="demo"></p></body></html>';
-console.log(start,page);
+// Code de Cristian
+var un = "Salut";
+var deux = un + "bonjour";
+var trois = 15;
+var quatre = trois - 5;
+var cinq = quatre * 9;
+var six = cinq / 3;
+var sept = six +2;
+console.log(un+" ",deux+" ",trois+" ",quatre+" ",cinq+" ",six+" ",sept+" ");
 
-// getElementById('demo').innerHTML= page;
+document.getElementById('demo').innerHTML= un+" "+deux+" "+trois+" "+quatre+" "+cinq+" "+six+" "+sept+" ";
+document.write("utilisation de write "+sept+" ", six+" ", cinq);
+
+// 
