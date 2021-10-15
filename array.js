a8a8e91639a852197d8743f1e1ce51c31bc5dfb1
akin <akin91.ay@gmail.com>
2021-10-15 10:41:43 +0200

    modification - exo akin tableau

--- array.js
+++ array.js
@@ -29,18 +29,11 @@ const pronoms = ["Je ","Tu ","Il/Elle/On ","Nous ","Vous ","Ils/Elles"]
 document.getElementById("nombres").innerHTML = nombres;
 document.getElementById("pronoms").innerHTML = pronoms;
 
+document.getElementById("ta1").innerHTML = string;
+document.getElementById("ta2").innerHTML = integer;
+document.getElementById("ta3").innerHTML = bool;
+document.getElementById("ta4").innerHTML = variable;
 
-
-var g1 = "groupe1"
-var g2 = "groupe2"
-var g3 = "groupe3"
-
-
-
-const string = ["rouge","vert","bleu"]
-const integer = [21,42,84]
-const bool = [true, false, null]
-const variable = [g1,g2,g3]
 
 
 
