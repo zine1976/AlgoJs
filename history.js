dd6b667fc0e182937c4097b1de1f9d33ca9d2d6e
Your Name <christian.tudoroiu@gmail.com>
2021-10-15 11:53:53 +0200

    [ADD] test des differentes boucles

--- history.js
+++ history.js
@@ -4,13 +4,14 @@
 //  pour chaque "mots" du "tableau":
 //        ecrire le mot     
 
-
-// Boucle Cristian for
-
+// Declaration variables
 const blague = ["C'est", " l'histoire  ","du", "ptit dej",", tu","la","connais", "?"];
 const reponse =["Pas", "de","bol."];
 let question = "";
 let repo = "";
+var indice = 0;
+
+// Boucle Cristian for
 
 for (let i = 0; i < blague.length; i++) {
     question +=blague[i]+" ";
@@ -22,7 +23,7 @@ for (let i = 0; i < blague.length; i++) {
   }
   document.getElementById("answer").innerHTML = repo;
 
-// Boucle For element in Cristian
+// Boucle For element of Cristian
 
 for (elem of blague){
     document.write(elem+" ");
@@ -31,4 +32,26 @@ document.write("<br>");
 
 for (rep of reponse){
     document.write(rep+" ");
+}
+document.write("<br>");
+
+
+// Boucle for avec conditions - Cristian
+for (let i in blague){
+    if (i%2 == 0){
+        document.write("cet element a un indice pair "+"<b>"+blague[i]+"</b>"+"<br>")
+    }
+    else{
+        document.write("cet element a un indice impair "+"<b>"+blague[i]+"</b>"+"<br>")
+
+    }
+}
+// Boucle while
+while ( indice < reponse.length ) {
+    document.write("<b>"+reponse[indice]+"</b>"+"<br>")
+    indice++;
+    if(reponse[indice] == "bol.")
+    {
+        reponse.push("HAHAHA , tres bonne blague")
+    }
 }
