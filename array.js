2056a07735e978a545ece31ed2e2fcc34d04b4c5
Michel <vilbertmichel9@gmail.com>
2021-10-15 10:14:34 +0200

    dazdaz

--- array.js
+++ array.js
@@ -1,13 +1,18 @@
 
-var g1 = "groupe1"
-var g2 = "groupe2"
-var g3 = "groupe3"
+// var g1 = "groupe1"
+// var g2 = "groupe2"
+// var g3 = "groupe3"
 
 
 
-const string = ["rouge","vert","bleu"]
-const integer = [21,42,84]
-const bool = [true, false, null]
-const variable = [g1,g2,g3]
+// const string = ["rouge","vert","bleu"]
+// const integer = [21,42,84]
+// const bool = [true, false, null]
+// const variable = [g1,g2,g3]
+
+
+const test =["jsp","jsp2"];
+let x = test[0];
+
 
 
