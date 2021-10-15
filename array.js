2fd0efdada82f13165c455e5a8ce056c249035f9
Gregor <failyriece@gmail.com>
2021-10-15 10:19:51 +0200

    [CHANGE] Place histoire

--- array.js
+++ array.js
@@ -36,8 +36,3 @@ const bool = [true, false, null]
 const variable = [g1,g2,g3]
 
 
-
-// Code d'el Roliste
-var textRoll = `Il était une lapine, qui un matin, c'est levée en voulant une carrote. "Oh, je voudrais tant manger votre carotte Monsieur!" dit-elle à un passant. Alors, le passant sorti sa carrote en lui précisant bien "Savoure petite, je te donne volontier ma grosse carotte". Ainsi, c'est avec le ventre bien plein que la jeune lapine commença la journée.`
-var tableauRoll = textRoll.split(' ');
-console.log(tableauRoll.join(' '));
